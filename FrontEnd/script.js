e4c7def34e18cc1b1963fdddf6814635cc59d86b
GiriGoutam <>
2019-04-06 20:54:34 +0530

    Created JS file

---
+++ FrontEnd/script.js
@@ -0,0 +1,36 @@
+function PlayItUp(config){
+	if(!config)
+        config = {};
+
+    let playItUp = {
+        user_id: config.user_id || 0,
+
+        init: function(){
+            let me = this;
+            me.bindUiEvents();
+            me.pageLoadEvents();
+        },
+        
+        bindUiEvents: function(){
+            let me = this;
+            console.log("ui event bindings");
+        },
+
+        pageLoadEvents:function(){
+            let me = this;
+            console.log("page load events");
+        },
+    }     
+
+    return playItUp;
+}
+
+$(document).ready(function(){
+	if(window.location.pathname.search('index')>0){		
+		let playItUp = new PlayItUp({
+			user_id: $("#user_id").val(),
+        });
+        
+        playItUp.init();
+	}
+})
